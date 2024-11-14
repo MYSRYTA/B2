b42532f4cb9279e2ae864945916c301b81f34d94
MYSRYTA <mysry.ta@gmail.com>
2024-11-15 06:08:15 +0900

    aaa

--- aaa.py
+++ aaa.py
@@ -131,3 +131,117 @@ def get_boundary_vertices_with_high_u_value(mesh_name, u_threshold=0.8):
             continue
 
     return high_u_vertices
+    
+import maya.api.OpenMaya as om
+
+def get_connected_uv_faces(mesh_name):
+    # メッシュのシェイプノードを取得
+    sel_list = om.MSelectionList()
+    sel_list.add(mesh_name)
+    dag_path = sel_list.getDagPath(0)
+    mesh_fn = om.MFnMesh(dag_path)
+    
+    # UVごとに接続するフェースのマッピングを作成
+    uv_face_map = {}
+    
+    # 全フェースをループして各フェースのUVを取得
+    for face_id in range(mesh_fn.numPolygons):
+        # フェースの頂点ごとにUVインデックスを取得
+        uv_ids = mesh_fn.getPolygonUVids(face_id)
+        
+        for uv_id in uv_ids:
+            # UVインデックスごとにフェースIDを追加
+            if uv_id not in uv_face_map:
+                uv_face_map[uv_id] = []
+            uv_face_map[uv_id].append(face_id)
+    
+    # 接続しているフェースのリストを作成
+    connected_face_groups = []
+    visited_faces = set()
+    
+    for uv_id, faces in uv_face_map.items():
+        connected_faces = []
+        
+        for face in faces:
+            if face not in visited_faces:
+                connected_faces.append(face)
+                visited_faces.add(face)
+        
+        if connected_faces:
+            connected_face_groups.append(connected_faces)
+    
+    return connected_face_groups
+
+# 使用例
+mesh_name = "pPlane1"  # メッシュの名前を指定
+connected_faces = get_connected_uv_faces(mesh_name)
+print("接続しているUVのフェース番号リスト:", connected_faces)
+
+import maya.cmds as cmds
+import maya.api.OpenMaya as om
+
+def get_border_vertices(mesh_name):
+    """メッシュの境界エッジにある頂点を取得し、左右の辺に分けて返します"""
+    sel_list = om.MSelectionList()
+    sel_list.add(mesh_name)
+    dag_path = sel_list.getDagPath(0)
+    
+    # メッシュのエッジイテレーターを使って境界エッジを特定
+    edge_it = om.MItMeshEdge(dag_path)
+    left_border = []
+    right_border = []
+    
+    while not edge_it.isDone():
+        if edge_it.onBoundary():
+            vtx1, vtx2 = edge_it.vertexId(0), edge_it.vertexId(1)
+            pos1 = om.MFnMesh(dag_path).getPoint(vtx1, om.MSpace.kWorld)
+            pos2 = om.MFnMesh(dag_path).getPoint(vtx2, om.MSpace.kWorld)
+            
+            # x座標に基づいて左と右の頂点を分類
+            if pos1.x < 0:
+                left_border.append(vtx1)
+            else:
+                right_border.append(vtx1)
+            
+            if pos2.x < 0:
+                left_border.append(vtx2)
+            else:
+                right_border.append(vtx2)
+        
+        edge_it.next()
+    
+    # 重複を削除してソート
+    left_border = sorted(set(left_border))
+    right_border = sorted(set(right_border))
+    return left_border, right_border
+
+def transfer_skin_weights(source_vertices, target_vertices, mesh_name):
+    """スキンウェイトを指定された頂点間で転写します"""
+    # スキンクラスタの取得
+    skin_cluster = cmds.ls(cmds.listHistory(mesh_name), type='skinCluster')[0]
+    influences = cmds.skinCluster(skin_cluster, query=True, influence=True)
+
+    # 各インフルエンスに対して、ウェイトを転写
+    for i, src_vtx in enumerate(source_vertices):
+        # ウェイトを取得
+        weights = cmds.skinPercent(skin_cluster, f"{mesh_name}.vtx[{src_vtx}]", query=True, value=True)
+        
+        # 反対側の頂点にウェイトを適用
+        tgt_vtx = target_vertices[i]
+        for j, influence in enumerate(influences):
+            cmds.skinPercent(skin_cluster, f"{mesh_name}.vtx[{tgt_vtx}]", transformValue=[(influence, weights[j])])
+
+def main(mesh_name):
+    # 境界頂点を取得し、左右の辺に分ける
+    left_border, right_border = get_border_vertices(mesh_name)
+    
+    # 左から右へのスキンウェイト転写
+    if len(left_border) == len(right_border):
+        transfer_skin_weights(left_border, right_border, mesh_name)
+        print("スキンウェイトの転写が完了しました。")
+    else:
+        print("エッジの頂点数が一致しません。手動で確認してください。")
+
+# 使用例
+mesh_name = "pPlane1"  # メッシュの名前を指定
+main(mesh_name)
